46cf330a9f9fa43ee7d3f93e042c3adfb5203cf1
Khin Me Me Latt <khinmemelatt@outlook.com>
2021-04-15 14:26:46 +0630

    Part1: Making Things Dynamic

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-Route::view('/', 'main');
-Route::view('/products', 'products');
+// Route::view('/', 'landing-page');
+Route::get('/','LandingPageController@index')->name('landing-page');
+Route::get('/shop','ShopController@index')->name('shop.index');
+Route::get('/shop/{product}','ShopController@show')->name('shop.show');
+// Route::view('/shop', 'shop');
 Route::view('/product', 'product');
 Route::view('/cart', 'cart');
 Route::view('/checkout', 'checkout');
