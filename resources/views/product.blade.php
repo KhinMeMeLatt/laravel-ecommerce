4f3799a03af42379f4f508f57d32b64c1b603a31
Khin Me Me Latt <khinmemelatt@outlook.com>
2021-05-17 22:24:25 +0630

    Image Gallery

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -21,13 +21,20 @@
     <div class="product-section container">
         <div>
             <div class="product-section-image">
-                <img src="{{ productImage($product->image) }}" alt="product">
+                <img src="{{ productImage($product->image) }}" alt="product" class="active" id="currentImage">
                 {{-- <img src="{{ asset('img/products/'.$product->slug.'.jpg') }}" alt="product"> --}}
             </div>
-            <div>
+            <div class="product-section-images">
+                
+                <div class="product-section-thumbnail selected">
+                    <img src="{{ productImage($product->image) }}" alt="product">
+                </div>
+
                 @if($product->images)
                     @foreach(json_decode($product->images, true) as $image)
-                        <img src="{{ productImage($image) }}" alt="product">
+                        <div class="product-section-thumbnail">
+                            <img src="{{ productImage($image) }}" alt="product">
+                        </div>
                     @endforeach
                 @endif
             </div>
@@ -56,4 +63,27 @@
     @include('partials.might-like')
 
 
+@endsection
+
+@section('extra-js')
+<script>
+    (function(){
+        const currentImage = document.querySelector('#currentImage');
+        const images = document.querySelectorAll('.product-section-thumbnail');
+
+        images.forEach((element) => element.addEventListener('click', thumbnailClick));
+        
+        function thumbnailClick(e) {
+            currentImage.classList.remove('active');
+
+            currentImage.addEventListener('transitionend', () => {
+                currentImage.src = this.querySelector('img').src;
+                currentImage.classList.add('active');
+            })
+
+            images.forEach((element) => element.classList.remove('selected'));
+            this.classList.add('selected');
+        }
+    })();
+</script>
 @endsection
