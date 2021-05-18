f7e083b872d1c12a41c166fa8c16457d25c0d7c5
Khin Me Me Latt <khinmemelatt@outlook.com>
2021-05-18 22:46:07 +0630

    User Authentication

---
+++ resources/views/partials/menus/main-right.blade.php
@@ -0,0 +1,21 @@
+<ul>
+    @guest
+        <li><a href="{{ route('register') }}">Sign Up</a></li>
+        <li><a href="{{ route('login') }}">Login</a></li>
+    @else
+        <li>
+            <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                Logout
+            </a>
+        </li>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    @endguest
+    <li><a href="{{ route('cart.index') }}">Cart
+        <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
+    </a></li>
+</ul>
