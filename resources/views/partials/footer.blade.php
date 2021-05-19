dbe08ff8269b3793670a2b06dbb6faddd9e3c83e
Khin Me Me Latt <khinmemelatt@outlook.com>
2021-05-20 00:04:04 +0630

    Change name in footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer-content container">
-        <div class="made-with">Made with <i class="fa fa-heart heart"></i> by Andre Madarang</div>
+        <div class="made-with">Made with <i class="fa fa-heart heart"></i> by Khin Me Me Latt</div>
         {{ menu('footer', 'partials.menus.footer') }}
     </div> <!-- end footer-content -->
 </footer>
