07f844e94229c68c222dea9a7bd39f5b4fcc959f
KhinMeMeLatt <khinmemelatt@uit.edu.mm>
2021-07-10 00:07:05 +0630

    Styling Auth Pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,73 +2,61 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-
-                                <a href="{{ route('guestCheckout.index') }}">Checkout as a Guest</a>
-                            </div>
-                        </div>
-                    </form>
+    <div class="auth-pages">
+        <div class="auth-left">
+            @if (session()->has('success_message'))
+                <div class="alert alert-success">
+                    {{ session()->get('success_message') }}
                 </div>
-            </div>
+            @endif
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <h2>Returning Customer</h2>
+            <div class="spacer"></div>
+
+            <form action="{{ route('login') }}" method="POST">
+                {{ csrf_field() }}
+
+                <input type="email" id="email" name="email" value="{{ old('email') }}" placeholder="Email" required autofocus>
+                <input type="password" id="password" name="password" value="{{ old('password') }}" placeholder="Password" required autofocus>
+
+                <div class="login-container">
+                    <button type="submit" class="auth-button">Login</button>
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    
+                    <label class="form-check-label" for="remember">
+                        {{ __('Remember Me') }}
+                    </label>
+                </div>
+
+                <div class="spacer"></div>
+
+                <a href="{{ route('password.request') }}">
+                    Forgot Your Password?
+                </a>
+            </form>
+        </div>
+        <div class="auth-right">
+            <h2>New Customer</h2>
+            <div class="spacer"></div>
+            <p><strong>Save time now.</strong></p>
+            <p>You don't need an account to checkout.</p>
+            <div class="spacer"></div>
+            <a href="{{ route('guestCheckout.index') }}" class="auth-button-hollow">Continue as Guest</a>
+            <div class="spacer"></div>
+            &nbsp;
+            <div class="spacer"></div>
+            <p><strong>Save time later.</strong></p>
+            <p>Create an account for fast checkout and easy access to order history.</p>
+            <div class="spacer"></div>
+            <a href="{{ route('register') }}" class="auth-button-hollow">Continue Account</a>
         </div>
     </div>
 </div>
