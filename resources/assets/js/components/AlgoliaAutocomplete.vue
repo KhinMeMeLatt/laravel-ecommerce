c51daa37015d12e73551d010590d8aa2a3cd43fd
KhinMeMeLatt <khinmemelatt@uit.edu.mm>
2021-07-11 22:49:44 +0630

    Algolia Autocomplete

--- resources/assets/js/components/AlgoliaAutocomplete.vue
+++ resources/assets/js/components/AlgoliaAutocomplete.vue
@@ -1,24 +1,11 @@
 <template>
-  <div class="aa-input-container" id="aa-input-container">
-    <input
-      type="search"
-      id="aa-search-input"
-      class="aa-input-search"
-      :placeholder="placeholder"
-      name="search"
-      autocomplete="off"
-    />
-    <svg class="aa-input-icon" viewBox="654 -372 1664 1664">
-      <path
-        d="M1806,332c0-123.3-43.8-228.8-131.5-316.5C1586.8-72.2,1481.3-116,1358-116s-228.8,43.8-316.5,131.5  C953.8,103.2,910,208.7,910,332s43.8,228.8,131.5,316.5C1129.2,736.2,1234.7,780,1358,780s228.8-43.8,316.5-131.5  C1762.2,560.8,1806,455.3,1806,332z M2318,1164c0,34.7-12.7,64.7-38,90s-55.3,38-90,38c-36,0-66-12.7-90-38l-343-342  c-119.3,82.7-252.3,124-399,124c-95.3,0-186.5-18.5-273.5-55.5s-162-87-225-150s-113-138-150-225S654,427.3,654,332  s18.5-186.5,55.5-273.5s87-162,150-225s138-113,225-150S1262.7-372,1358-372s186.5,18.5,273.5,55.5s162,87,225,150s113,138,150,225  S2062,236.7,2062,332c0,146.7-41.3,279.7-124,399l343,343C2305.7,1098.7,2318,1128.7,2318,1164z"
-      />
-    </svg>
-  </div>
+  <div id="autocomplete"></div>
 </template>
 
 <script>
 import algoliasearch from "algoliasearch/lite";
-import { autocomplete } from "@algolia/autocomplete-js";
+import { autocomplete, getAlgoliaResults } from "@algolia/autocomplete-js";
+import "@algolia/autocomplete-theme-classic";
 
 export default {
   props: {
@@ -32,65 +19,78 @@ export default {
     },
   },
   mounted() {
-    var client = algoliasearch(
-      process.env.ALGOLIA_APP_ID,
-      process.env.ALGOLIA_API_KEY
+    var searchClient = algoliasearch(
+      process.env.MIX_ALGOLIA_APP_ID,
+      process.env.MIX_ALGOLIA_API_KEY
     );
-    var index = client.initIndex(this.indexName);
+// var enterPressed = false;
+    // var index = searchClient.initIndex(this.indexName);
     var enterPressed = false;
-    //initialize autocomplete on search input (ID selector must match)
-    autocomplete(
-      "#aa-search-input",
-      { hint: false },
-      {
-        source: autocomplete.sources.hits(index, { hitsPerPage: 10 }),
-        //value to be displayed in input control after user's suggestion selection
-        displayKey: "name",
-        //hash of templates used when rendering dataset
-        templates: {
-          //'suggestion' templating function used to render a single suggestion
-          suggestion: function (suggestion) {
-            const markup = `
-                                <div class="algolia-result">
-                                    <span>
-                                        <img src="${window.location.origin}/storage/${suggestion.image}" alt="img" class="algolia-thumb">
-                                        ${
-                                          suggestion._highlightResult.name.value
-                                        }
-                                    </span>
-                                    <span>$${(suggestion.price / 100).toFixed(
-                                      2
-                                    )}</span>
-                                </div>
-                                <div class="algolia-details">
-                                    <span>${
-                                      suggestion._highlightResult.details.value
-                                    }</span>
-                                </div>
-                            `;
-            return markup;
+    autocomplete({
+      container: "#autocomplete",
+      placeholder: this.placeholder,
+      getSources({ query }) {
+        return [
+          {
+            sourceId: "products",
+            getItems() {
+              return getAlgoliaResults({
+                searchClient,
+                queries: [
+                  {
+                    indexName: "products",
+                    query,
+                    params: {
+                      hitsPerPage: 5,
+                      attributesToSnippet: ["name:10", "description:35"],
+                      snippetEllipsisText: "…",
+                    },
+                  },
+                ],
+              });
+            },
+            getItemInputValue({ item }) {
+              return item.name;
+            },
+            templates: {
+              item({ item, createElement }) {
+                return createElement("div", {
+                  dangerouslySetInnerHTML: {
+                    __html: `<div class="algolia-result">
+                <span>
+                  <img
+                    src="/storage/${item.image}"
+                    alt="${item.name}"
+                    class="algolia-thumb"
+                  />
+                   ${item.name}
+                </span>
+                <span>
+                $${(item.price / 100).toFixed(2)}
+                </span>
+                </div>
+                <div class="algolia-details">
+                  <span>
+                    ${item.details}
+                  </span>
+                </div>`,
+                  },
+                });
+              },
+              noResults() {
+                return 'Sorry, we did not find any results for '+document.getElementById("autocomplete-0-input").value;
+              },
+            },
+            getItemUrl({ item }) {
+              enterPressed = true;
+              return "/shop/"+item.slug;
+            },
+                       
           },
-          empty: function (result) {
-            return (
-              'Sorry, we did not find any results for "' + result.query + '"'
-            );
-          },
-        },
-      }
-    )
-      .on("autocomplete:selected", function (event, suggestion, dataset) {
-        window.location.href =
-          window.location.origin + "/shop/" + suggestion.slug;
-        enterPressed = true;
-      })
-      .on("keyup", function (event) {
-        if (event.keyCode == 13 && !enterPressed) {
-          window.location.href =
-            window.location.origin +
-            "/search-algolia?q=" +
-            document.getElementById("aa-search-input").value;
-        }
-      });
+        ];
+      },
+     
+    });
   },
 };
 </script>
