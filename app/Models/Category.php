3b04aa5855e60daf019e10d0faf64e14d64193f3
KhinMeMeLatt <khinmemelatt@uit.edu.mm>
2021-07-22 23:29:08 +0630

    Fix errors

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'slug'];
     protected $table = 'category';
     public function products()
     {
